cc4216e3b4ebb6ebc577572e36541d362717298c
kellyeryan <kellyeryan@gmail.com>
2019-06-28 12:58:19 -0400

    Finish Learn Konami Lab

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,34 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
-
 function init() {
-  // your code here
+  const codes = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
+  ]
+
+  let index = 0
+  document.body.addEventListener('keydown', onKeyDownHandler)
+
+  function onKeyDownHandler(e) {
+    const key = e.key
+
+    if (key === codes[index]) {
+      index++;
+
+      if (index === codes.length) {
+        alert("Congratulations!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  }
 }
+
+
